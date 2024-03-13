3dc620ab067db099a115c512278f40084499d986
Nensi-Sonani <nrsonani13@gmail.com>
2024-03-13 17:17:55 +0530

    review2

--- src/ReviewForm.jsx
+++ src/ReviewForm.jsx
@@ -22,23 +22,31 @@ const ReviewForm = () => {
         <div>
             <div className='main-head'><div className='head-name'> Give Review Here</div></div>
             <form action="" onSubmit={handleSubmit} className='reviewform'>
-                <label htmlFor="">Title:</label>
+                <div>
+                <label htmlFor="">Title : </label>
                 <input type="text" placeholder='Please Enter The Title' onChange={(e) => setTitle(e.target.value)} required />
+                </div>
+                <div>
+                <label htmlFor="">Rating:</label>
                 <input type="number" placeholder='Enter rating between 1 to 5' onChange={(e) => setRating(e.target.value)} />
-                <textarea name="" id="" cols="30" rows="5" placeholder='Enter Description' onChange={(e) => setDescription(e.target.value)}></textarea>
+                </div>
+                <div>
+                <label htmlFor="">Description:</label>
+                <textarea name="" id="" cols="36" rows="5" placeholder='Enter Description' onChange={(e) => setDescription(e.target.value)}></textarea>
+                </div>
                 <input type="submit" name="" id="" />
                 <input type="reset" name="" id="" />
             </form>
             <div className='main-head'><div className='head-name'>User Review</div></div>
 
-            <div className='review-main'>
+            <div className='review-main container'>
             {
                 data.map((e, id) => {
                     return (
-                          <div key={id} className=''>
-                            <p>{e.title}</p>
-                            <p>{e.rating}</p>
-                            <p>{e.description}</p>
+                          <div key={id} className='review'>
+                            <p className=''><label htmlFor="">Title:</label><span>{e.title}</span></p>
+                            <p> <label htmlFor="">Rating:</label><span>{e.rating}</span></p>
+                            <p><label htmlFor="">Des:</label><span>{e.description}</span></p>
                         </div>
                     )
                 })
